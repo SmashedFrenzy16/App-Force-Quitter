d1263791ed184cd696d0cfb3bd53df1a2f9d7ed1
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-20 10:56:48 +0100

    Update force_quit_apps.py with entry field

--- force_quit_apps.py
+++ force_quit_apps.py
@@ -9,4 +9,9 @@ label.pack()
 blank_label = Label(root, text="")
 blank_label.pack()
 
+app_entry = Entry(root, width=100, borderwidth=5)
+app_entry.pack()
+
+app_entry.insert(0, "Enter the name of the app you want to force quit")
+
 root.mainloop()
