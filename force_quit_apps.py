30573080c9cb6fefbc0648b7f8c108954133e355
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-20 10:48:38 +0100

    Update force_quit_apps.py with title

--- force_quit_apps.py
+++ force_quit_apps.py
@@ -3,4 +3,10 @@ from tkinter import *
 root = Tk()
 root.title("Force Quit Apps By SmashedFrenzy16")
 
+label = Label(root, text="Force Quit Apps By @SmashedFrenzy16", font=("Arial", 24))
+label.pack()
+
+blank_label = Label(root, text="")
+blank_label.pack()
+
 root.mainloop()
